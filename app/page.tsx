e013608cdcb9826977cc2b5df3937c519695f2d2
Ruchi Ray <ruchiray18@gmail.com>
2024-03-07 20:47:54 +0530

    navbar completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { TopStrip } from "@/components/TopStrip/TopStrip";
 import "../commonStyles/utilities.css";
+import { Navbar } from "@/components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <main className="w-full">
-      <TopStrip />
+      {/* <TopStrip /> */}
+      <Navbar />
     </main>
   );
 }
---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,70 @@
+"use client";
+import React, { useState } from "react";
+import "./Navbar.css";
+import Image from "next/image";
+import { CiSearch } from "react-icons/ci";
+import { BsBagDash } from "react-icons/bs";
+import { IoHeartOutline } from "react-icons/io5";
+import { IoIosArrowDown } from "react-icons/io";
+import { HiOutlineUser } from "react-icons/hi2";
+import { MdMenu } from "react-icons/md";
+import { IoMdClose } from "react-icons/io";
+export const Navbar = () => {
+  const [showMenu, setshowMenu] = useState(false);
+  return (
+    <>
+      <div className="navbar-wrapper flex justify-between">
+        <div className=" centerContainer ">
+          <button
+            onClick={() => setshowMenu(true)}
+            className="menu-icon center"
+          >
+            <MdMenu size={30} />
+          </button>
+          <div className="logo">
+            <Image
+              src={"/images/Logo.png"}
+              alt="logo"
+              width={32}
+              height={32}
+              className="logo"
+              layout="responsive"
+            />
+          </div>
+        </div>
+        <h1 className="text-center LOGO">LOGO</h1>
+        <div className="icons-wrapper flex align-center">
+          <CiSearch />
+          <IoHeartOutline fontWeight={300} />
+          <BsBagDash fontSize={20} />
+          <HiOutlineUser className="hide" fontSize={22} />
+          <div className="lang hide flex align-center">
+            <p className="font-700">ENG</p>
+            <IoIosArrowDown />
+          </div>
+        </div>
+      </div>
+      <div
+        className={`menu-wrapper justify-center font-500 flex-wrap flex ${
+          showMenu && "show-menu"
+        }`}
+      >
+        <button onClick={() => setshowMenu(false)} className="close-icon show">
+          <IoMdClose size={30} />
+        </button>
+        <p>SHOP</p>
+        <p>SKILLS</p>
+        <p>STORIES</p>
+        <p>ABOUT </p>
+        <p>CONTACT US</p>
+        <div className="flex show align-center">
+          <HiOutlineUser fontSize={22} />
+          <div className="lang flex align-center">
+            <p className="font-700">ENG</p>
+            <IoIosArrowDown />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
