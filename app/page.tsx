579f8f1b760233ce96684477414a70f8dd5d691e
Ruchi Ray <ruchiray18@gmail.com>
2024-03-08 00:51:30 +0530

    hero completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { TopStrip } from "@/components/TopStrip/TopStrip";
 import "../commonStyles/utilities.css";
 import { Navbar } from "@/components/Navbar/Navbar";
+import { Hero } from "@/components/Hero/Hero";
 
 export default function Home() {
   return (
     <main className="w-full">
       {/* <TopStrip /> */}
       <Navbar />
+      <Hero />
     </main>
   );
 }
---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Hero.css";
+export const Hero = () => {
+  return (
+    <div className="hero-wrapper w-full text-center flex flex-col">
+      <h1 className="font-400 hero-header">DISCOVER OUR PRODUCTS</h1>
+      <div className="hero-desc flex flex-col">
+        <p>Lorem ipsum dolor sit amet consectetur. Amet est posuere rhoncus</p>
+        <p>
+          scelerisque. Dolor integer scelerisque nibh amet mi ut elementum
+          dolor.
+        </p>
+      </div>
+    </div>
+  );
+};
